c9e5207ca708e9d6a056e186f60fccc700ae23e2
Rishat <rishatfattahov22@gmail.com>
2026-03-11 20:40:13 +0300

    create webapp7

--- webapp/app.js
+++ webapp/app.js
@@ -5,18 +5,63 @@ tg.expand()
 
 const user = tg.initDataUnsafe?.user
 
-const userDiv = document.getElementById("user")
+const app = document.getElementById("app")
 
 if(!user){
 
- userDiv.innerHTML = "Откройте приложение через Telegram"
+ app.innerHTML="Откройте Mini App через Telegram"
 
-}else{
+ throw new Error("No telegram user")
 
- userDiv.innerHTML = `
- <p>ID: ${user.id}</p>
- <p>Имя: ${user.first_name}</p>
- <p>Username: ${user.username}</p>
- `
+}
+
+const userId = user.id
+
+init()
+
+async function init(){
+
+ // проверка регистрации
+ const res = await fetch("/api/checkUser",{
+  method:"POST",
+  headers:{
+   "Content-Type":"application/json"
+  },
+  body:JSON.stringify({userId})
+ })
+
+ const data = await res.json()
+
+ if(!data.registered){
+
+  app.innerHTML = `
+  <h2>Вы не зарегистрированы</h2>
+  <p>Напишите боту команду /start</p>
+  `
+  return
+
+ }
+
+ // проверяем админа
+
+ const adminCheck = await fetch("/api/checkAdmin",{
+  method:"POST",
+  headers:{
+   "Content-Type":"application/json"
+  },
+  body:JSON.stringify({userId})
+ })
+
+ const adminData = await adminCheck.json()
+
+ if(adminData.admin){
+
+  window.location="admin.html"
+
+ }else{
+
+  window.location="user.html"
+
+ }
 
 }
