074f812fa79ea01d870bc611a5d942e2b69ce51e
Rishat <rishatfattahov22@gmail.com>
2026-03-12 05:20:29 +0300

    create webapp47

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose")
 
-mongoose.connect(process.env.MONGO_URI)
+const uri = process.env.MONGO_URI
+
+if(!uri){
+ throw new Error("❌ MONGO_URI не задан")
+}
+
+mongoose.connect(uri)
 
 mongoose.connection.on("connected", () => {
  console.log("✅ MongoDB connected")
