794d73ff7c02a7a8ebbbcc726ed0fceb91070bfe
Rishat <rishatfattahov22@gmail.com>
2026-03-12 05:14:24 +0300

    create webapp46

--- db.js
+++ db.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose")
 
-mongoose.connect(process.env.MONGO_URI,{
- useNewUrlParser:true,
- useUnifiedTopology:true
+mongoose.connect(process.env.MONGO_URI)
+
+mongoose.connection.on("connected", () => {
+ console.log("✅ MongoDB connected")
 })
 
-const db = mongoose.connection
-
-db.on("error",console.error)
-db.once("open",()=>{
- console.log("✅ MongoDB connected")
+mongoose.connection.on("error", (err) => {
+ console.log("❌ MongoDB error:", err)
 })
 
 module.exports = mongoose
